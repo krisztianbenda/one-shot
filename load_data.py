ee221ce57a72eb0abc57de288f979af1959c6c52
krisztianbenda <krisztianbenda@gmail.com>
2017-11-08 11:18:44 +0100

    some MOD

--- load_data.py
+++ load_data.py
@@ -28,24 +28,25 @@ def loadimgs(path,n=0):
         lang_dict[alphabet] = [curr_y,None]
         alphabet_path = os.path.join(path,alphabet)
         #every letter/category has it's own column in the array, so  load seperately
-        for letter in os.listdir(alphabet_path):
-            cat_dict[curr_y] = (alphabet, letter)
-            category_images=[]
-            letter_path = os.path.join(alphabet_path, letter)
-            if "DS_Store" not in letter_path:
-                for filename in os.listdir(letter_path):
-                    image_path = os.path.join(letter_path, filename)
-                    image = imread(image_path)
-                    category_images.append(image)
-                    y.append(curr_y)
-                try:
-                    X.append(np.stack(category_images))
-                #edge case  - last one
-                except ValueError as e:
-                    print(e)
-                    print("error - category_images:", category_images)
-                curr_y += 1
-                lang_dict[alphabet][1] = curr_y - 1
+        if "DS_Store" not in alphabet_path:
+            for letter in os.listdir(alphabet_path):
+                cat_dict[curr_y] = (alphabet, letter)
+                category_images=[]
+                letter_path = os.path.join(alphabet_path, letter)
+                if "DS_Store" not in letter_path:
+                    for filename in os.listdir(letter_path):
+                        image_path = os.path.join(letter_path, filename)
+                        image = imread(image_path)
+                        category_images.append(image)
+                        y.append(curr_y)
+                    try:
+                        X.append(np.stack(category_images))
+                    #edge case  - last one
+                    except ValueError as e:
+                        print(e)
+                        print("error - category_images:", category_images)
+                    curr_y += 1
+                    lang_dict[alphabet][1] = curr_y - 1
     y = np.vstack(y)
     X = np.stack(X)
     return X,y,lang_dict
